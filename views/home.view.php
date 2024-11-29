9ffbd99e42c2166788e4af17f4aab5de0e9dcbbf
Hlxuan <huanglangxuan@hlxuan.top>
2024-11-29 11:18:11 +0800

    update:调整home.view.php文件顶部横幅代码引入方式

--- views/home.view.php
+++ views/home.view.php
@@ -4,15 +4,7 @@
 
 <?php require basePath("views/partials/showcase-search.php"); ?>
 
-<!-- 顶部横幅 -->
-<section class="bg-blue-900 text-white py-6 text-center">
-    <div class="container mx-auto">
-        <h2 class="text-3xl font-semibold">解锁你的职业潜能</h2>
-        <p class="text-lg mt-2">
-            发现完美的实习机会
-        </p>
-    </div>
-</section>
+<?php require basePath("views/partials/top-banner.php"); ?>
 
 <!-- 实习列表 -->
 <section>
