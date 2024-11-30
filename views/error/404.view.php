5c8d016aa56b4650961fc4b17052dd9d4c224000
Hlxuan <huanglangxuan@hlxuan.top>
2024-11-30 20:43:12 +0800

    feat:404视图的实现

---
+++ views/error/404.view.php
@@ -0,0 +1,19 @@
+<?php loadPartial("head"); ?>
+
+<?php loadPartial("navbar"); ?>
+
+<?php loadPartial("top-banner"); ?>
+
+<!-- 错误信息 -->
+<section>
+    <div class="container mx-auto p-4 mt-4">
+        <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">404错误</div>
+        <p class="text-center text-2xl mb-4">
+            此页面不存在
+        </p>
+    </div>
+</section>
+
+<?php loadPartial("bottom-banner"); ?>
+
+<?php loadPartial("footer") ?>
