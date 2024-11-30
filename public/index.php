4ef14b75d1a32155d4e9a621cd3c077f6e9a95bf
Hlxuan <huanglangxuan@hlxuan.top>
2024-11-30 15:41:28 +0800

    feat:实现初步路由逻辑

--- public/index.php
+++ public/index.php
@@ -1,4 +1,13 @@
 <?php
 
 require "../helpers.php";
-loadView("home");
+
+$routes = [
+    "/" => "controllers/home.php",
+    "/listings" => "controllers/listings.php",
+    "/listings/create" => "controllers/listings_create.php",
+    "404" => "controllers/404.php"
+];
+
+$uri = $_SERVER["REQUEST_URI"];
+inspectAndDie($uri);
