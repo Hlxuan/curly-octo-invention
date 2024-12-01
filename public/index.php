fd86b6abd6757e4a1653200e1c78ad35bdda33b7
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-01 20:14:27 +0800

    delete:删除index.php数据库相关语句

--- public/index.php
+++ public/index.php
@@ -1,9 +1,6 @@
 <?php
 require "../helpers.php";
 require basePath("Database.php");
-$config = require basePath("config/db.php");
-
-$db = new Database($config);
 
 require basePath("router.php");
 
