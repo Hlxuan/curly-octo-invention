3021db978002f68759380689963f633f1e3e489c
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-01 19:49:37 +0800

    update:新增通用查询方法

--- Database.php
+++ Database.php
@@ -23,4 +23,21 @@ class Database
             throw new Exception("数据库连接失败：{$e->getMessage()}");
         }
     }
+
+    /**
+     * 执行数据库请求
+     * @param string $query
+     * @return PDOStatement
+     * @throws PDOException
+     */
+    public function query($query)
+    {
+        try {
+            $sth = $this->conn->prepare($query);
+            $sth->execute();
+            return $sth;
+        } catch (PDOException $e) {
+            throw new Exception("数据库请求执行失败：{$e->getMessage()}");
+        }
+    }
 }
